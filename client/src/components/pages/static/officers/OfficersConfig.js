33b459d2db4c1545cc2585e1ae99ade02fb9a6fb
itzdonaldxd <46495615+itzdonaldxd@users.noreply.github.com>
2021-08-11 16:18:16 -0400

    Fixed photo formats

--- client/src/components/pages/static/officers/OfficersConfig.js
+++ client/src/components/pages/static/officers/OfficersConfig.js
@@ -2,62 +2,62 @@ const officerOptions = [
   {
     position: "Show Coordinator",
     name: "Xochitl Luna",
-    photoUrl: "/officer_images/Xochitl.jpeg",
+    photoUrl: "/officer_images/Xochitl.jpg",
   },
   {
     position: "President",
     name: "Amber Zheng",
-    photoUrl: "/officer_images/Amber.jpeg",
+    photoUrl: "/officer_images/Amber.jpg",
   },
   {
     position: "Vice President",
     name: "Sage Simhon",
-    photoUrl: "/officer_images/Sage.jpeg",
+    photoUrl: "/officer_images/Sage.jpg",
   },
   {
     position: "Treasurer",
     name: "Maya Redden",
-    photoUrl: "/officer_images/Maya.jpeg",
+    photoUrl: "/officer_images/Maya.jpg",
   },
   {
     position: "Pub Chair",
     name: "Jade Ishii",
-    photoUrl: "/officer_images/Jade.jpeg",
+    photoUrl: "/officer_images/Jade.jpg",
   },
   {
     position: "Pub Chair",
     name: "Irene Terpstra",
-    photoUrl: "/officer_images/Irene.jpeg",
+    photoUrl: "/officer_images/Irene.jpg",
   },
   {
     position: "Secretary",
     name: "Maryann Chidume",
-    photoUrl: "/officer_images/Maryann.jpeg",
+    photoUrl: "/officer_images/Maryann.jpg",
   },
   {
     position: "Webmaster",
     name: "Donald Liu",
-    photoUrl: "/officer_images/Donald.jpeg",
+    photoUrl: "/officer_images/Donald.jpg",
   },
   {
     position: "Social Chair",
     name: "Quentin Thernize",
-    photoUrl: "/officer_images/Quentin.jpeg",
+    photoUrl: "/officer_images/Quentin.jpg",
   },
   {
     position: "Media Historian",
     name: "Eva Smerekanych",
-    photoUrl: "/officer_images/Eva.jpeg",
+    photoUrl: "/officer_images/Eva.jpg",
   },
   {
     position: "Tech Coordinator",
     name: "Montse Garza",
-    photoUrl: "/officer_images/Montse.jpeg",
+    photoUrl: "/officer_images/Montse.jpg",
   },
   {
     position: "Costume Coordinator",
     name: "Maria Fedyk",
-    photoUrl: "/officer_images/Maria.jpeg",
+    photoUrl: "/officer_images/Maria.jpg",
   },
 ];
 
