34088ecbbe2fb2523607f38a046283fcd7d4a014
Konnie Duan <konnie@mit.edu>
2025-02-02 14:34:31 -0800

    add sp 2025 audition info

--- client/src/components/pages/static/Auditions.js
+++ client/src/components/pages/static/Auditions.js
@@ -171,8 +171,7 @@ class Auditions extends Component {
           <h1>Auditions</h1>
           <h2>TLDR</h2>
           <h3>
-            <b>Date:</b> September 7th and 8th, 2024<br />
-            <b>Location:</b> Lobby 13 (Saturday) and BC Porter Room (Sunday)<br />
+            <b>Date:</b> February 8th and 9th, 2025<br />
             <b>Saturday:</b> Choreographer showcases and preference sheet submission<br />
             <b>Sunday:</b> Audition day with different styles at specific times (Tap, Contemporary, Ballet, Hip-Hop)<br />
             <b>Results:</b> Announced Sunday evening, practices begin Monday<br />
@@ -181,7 +180,7 @@ class Auditions extends Component {
 
           <p>
             DanceTroupe auditions are divided between a Saturday and a Sunday at
-            the beginning of each term. For <b>Fall 2024</b>, they will be held on <b><u>September 7th and 8th, 2024</u></b> in <b><u>Lobby 13</u></b> and the <b><u>BC Porter Room</u></b>, respectively. 
+            the beginning of each term. For <b>Spring 2025</b>, they will be held on <b><u>February 8th and 9th, 2025</u></b> in <b><u>Lobby 13</u></b> and the <b><u>Mccormick for ballet and BC Porter Room for all other styles</u></b>, respectively. 
             Auditions are a laid-back, fun process, so nothing should stop you
             from auditioning!
           </p>
